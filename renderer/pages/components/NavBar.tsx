637e03a584c634c1caaf555952d387b0efd5db38
mamonde456 <mamonde456@gmail.com>
2023-01-26 20:06:45 +0900

    add: 네비게이션

---
+++ renderer/pages/components/NavBar.tsx
@@ -0,0 +1,46 @@
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { auth } from "../../../firebase";
+
+export default function NavBar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [email, setEmail] = useState("");
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+        console.log(user);
+        const userName = user.email.split("@");
+        setEmail(userName[0]);
+      } else {
+        setIsLoggedIn(false);
+      }
+    });
+  }, []);
+
+  const onLogout = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("로그아웃 완료");
+      })
+      .catch((error) => {
+        console.log("로그아웃실패");
+      });
+  };
+  return (
+    <>
+      <Link href="/">home</Link>
+      {isLoggedIn ? (
+        <>
+          {" "}
+          <span onClick={onLogout}>logout</span>
+          <Link href="/chat">Chat room</Link>
+        </>
+      ) : (
+        <Link href="/login">login</Link>
+      )}
+      {isLoggedIn && <p>어서오세요. {email} 님</p>}
+    </>
+  );
+}
