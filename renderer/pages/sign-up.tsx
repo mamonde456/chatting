0d8cc400163bf6f603dd6462525f82303b1b361b
mamonde456 <mamonde456@gmail.com>
2023-01-30 16:18:08 +0900

    add: 유저 회원가입 시 name 목록 추가

--- renderer/pages/sign-up.tsx
+++ renderer/pages/sign-up.tsx
@@ -2,14 +2,16 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
-import { auth } from "../../firebase";
+import { auth, db } from "../../firebase";
 import { emailMatch, passwordMatch } from "../until";
 
 export default function SignUp() {
   const [user, setUser] = useState({
+    name: "",
     email: "",
     password: "",
   });
@@ -24,9 +26,16 @@ export default function SignUp() {
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     createUserWithEmailAndPassword(auth, user.email, user.password)
-      .then((userCredential) => {
+      .then(async (userCredential) => {
         console.log(userCredential);
-        return router.push("/login");
+        await setDoc(doc(db, "users", userCredential.user.uid), {
+          name: user.name,
+          email: userCredential.user.email,
+          createdAt: Date.now(),
+          id: userCredential.user.uid,
+        });
+
+        return router.push("/");
       })
       .catch((error) => {
         const errorMessage = error.message;
@@ -35,9 +44,26 @@ export default function SignUp() {
   };
   return (
     <form onSubmit={onSubmit}>
-      <input type="text" name="email" onInput={onChange} value={user.email} />
+      <label htmlFor="name">name</label>
       <input
         type="text"
+        id="name"
+        name="name"
+        onInput={onChange}
+        value={user.name}
+      />
+      <label htmlFor="email">email</label>
+      <input
+        type="text"
+        id="email"
+        name="email"
+        onInput={onChange}
+        value={user.email}
+      />
+      <label htmlFor="password">password</label>
+      <input
+        type="text"
+        id="password"
         name="password"
         onInput={onChange}
         value={user.password}
@@ -49,7 +75,7 @@ export default function SignUp() {
           passwordMatch(user.password) && emailMatch(user.email) ? false : true
         }
       />
-      <Link href="/sign-up">Login</Link>
+      <Link href="/login">Login</Link>
     </form>
   );
 }
