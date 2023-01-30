7d293a4fd04f74882dd5019ca4e23f605650ffe8
mamonde456 <mamonde456@gmail.com>
2023-01-30 16:15:08 +0900

    remove: 불필요한 부분 삭제

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-  mainSrcDir: "main",
-  rendererSrcDir: "renderer",
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.target = ["web", "es6"];
     }
-    console.log("2 ", config.target, isServer);
     return config;
   },
 };
