d9c2ac265c36b6a17d8ef82b1f9a732f3858de6c
mamonde456 <mamonde456@gmail.com>
2023-01-31 17:16:08 +0900

    add: 채팅방 스타일 작성

--- renderer/pages/chat/[id]/index.tsx
+++ renderer/pages/chat/[id]/index.tsx
@@ -14,7 +14,69 @@ import {
 } from "firebase/firestore";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 import { auth, db } from "../../../../firebase";
+
+const MsgList = styled.ul`
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  li {
+    /* max-width: 200px; */
+    /* width: 100%; */
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding: 10px;
+    .meRight {
+      align-items: flex-end;
+    }
+    .youLeft {
+      align-items: flex-start;
+    }
+    .name {
+      margin-bottom: 10px;
+    }
+  }
+`;
+
+const MessageBox = styled.div`
+  .message {
+    border-radius: 8px;
+    max-width: 250px;
+    padding: 10px;
+    background-color: #cdcdcd;
+    position: relative;
+  }
+  .meArrow {
+    &::before {
+      content: "";
+      position: absolute;
+      right: -18px;
+      top: 8px;
+      width: 0;
+      height: 0;
+      border-bottom: 8px solid transparent;
+      border-top: 8px solid transparent;
+      border-left: 10px solid #cdcdcd;
+      border-right: 10px solid transparent;
+    }
+  }
+  .youArrow {
+    &::before {
+      content: "";
+      position: absolute;
+      left: -18px;
+      top: 8px;
+      width: 0;
+      height: 0;
+      border-bottom: 8px solid transparent;
+      border-top: 8px solid transparent;
+      border-left: 10px solid transparent;
+      border-right: 10px solid #cdcdcd;
+    }
+  }
+`;
 
 export default function ChattingRoom() {
   const [user, setUser] = useState({
@@ -41,28 +103,28 @@ export default function ChattingRoom() {
     onAuthStateChanged(auth, (user) => {
       setUser({ email: user.email, id: user.uid });
     });
-    enterUsers();
+    // enterUsers();
   }, []);
 
-  useEffect(() => {
-    const newMessageQuery = query(
-      collection(db, `room-${id}`),
-      orderBy("createdAt")
-    );
-    // const unsubscribe = onSnapshot(collection(db, `room-${id}`), (snapshot) => {
-    //   snapshot.forEach((doc) => {
-    //     console.log(doc.data());
-    //     setMessages((prev) => [doc.data(), ...prev]);
-    //   });
-    // });
-    return onSnapshot(newMessageQuery, (snapshot) => {
-      setMessages([]);
-      snapshot.forEach((doc) => {
-        setMessages((prev) => [...prev, doc.data()]);
-      });
-    });
-    // return unsubscribe();
-  }, []);
+  // useEffect(() => {
+  //   const newMessageQuery = query(
+  //     collection(db, `room-${id}`),
+  //     orderBy("createdAt")
+  //   );
+  //   // const unsubscribe = onSnapshot(collection(db, `room-${id}`), (snapshot) => {
+  //   //   snapshot.forEach((doc) => {
+  //   //     console.log(doc.data());
+  //   //     setMessages((prev) => [doc.data(), ...prev]);
+  //   //   });
+  //   // });
+  //   return onSnapshot(newMessageQuery, (snapshot) => {
+  //     setMessages([]);
+  //     snapshot.forEach((doc) => {
+  //       setMessages((prev) => [...prev, doc.data()]);
+  //     });
+  //   });
+  //   // return unsubscribe();
+  // }, []);
 
   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
     const {
@@ -94,14 +156,33 @@ export default function ChattingRoom() {
 
   return (
     <div>
-      <div>
-        {messages?.map((el) => (
-          <div key={el.id}>
+      <div>{}</div>
+      <MsgList>
+        {/* {messages?.map((el) => (
+          <div key={el.createdAt}>
             <span>{el.owner.name.split("@")[0]}</span>
             <span>{el.message}</span>
           </div>
-        ))}
-      </div>
+        ))} */}
+        <li className="meRight">
+          <span className="name">garam</span>
+          <MessageBox>
+            <span className="message">dafsdf</span>
+          </MessageBox>
+        </li>
+        <li className="meRight">
+          <span className="name">garam</span>
+          <MessageBox>
+            <span className="message meArrow">dafsdf</span>
+          </MessageBox>
+        </li>
+        <li className="youLeft">
+          <span className="name">test</span>
+          <MessageBox>
+            <span className="message youArrow">dafsdf</span>
+          </MessageBox>
+        </li>
+      </MsgList>
 
       <form>
         <input type="text" name="message" onChange={onChange} value={message} />
