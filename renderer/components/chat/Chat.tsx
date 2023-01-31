caf86e6fc5aa1aa487d51ea395c3726724c3f17d
mamonde456 <mamonde456@gmail.com>
2023-01-31 17:18:06 +0900

    add: 메세지 보여주는 컴포넌트 분리

---
+++ renderer/components/chat/Chat.tsx
@@ -0,0 +1,55 @@
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { db } from "../../../firebase";
+
+const TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  .chatting {
+    color: rgba(0, 0, 0, 0.5);
+  }
+`;
+export default function Chat({ roomName, roomId }: any) {
+  const [messages, setMessages] = useState([]);
+  const router = useRouter();
+  const getMessages = () => {
+    try {
+      const newMessageQuery = query(
+        collection(db, `room-${roomId}`),
+        orderBy("createdAt")
+      );
+      return onSnapshot(newMessageQuery, (snapshot) => {
+        setMessages([]);
+        snapshot.forEach((doc) => {
+          setMessages((prev) => [...prev, doc.data()]);
+        });
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  useEffect(() => {
+    getMessages();
+  }, []);
+
+  return (
+    <li onClick={() => router.push(`chat/${roomId}`)}>
+      <div className="avatar"></div>
+      <TextBox>
+        <span>{roomName}</span>
+        <span className="chatting">
+          {messages[messages.length - 1]?.message}
+        </span>
+      </TextBox>
+    </li>
+  );
+}
