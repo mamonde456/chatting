420d4b0a999ad1ee227e012a057ed8d29b357dd2
mamonde456 <mamonde456@gmail.com>
2023-02-01 14:40:25 +0900

    update: 로그인 스타일 추가 및 label 구조 추가, 에러 처리

--- renderer/pages/sign-up.tsx
+++ renderer/pages/sign-up.tsx
@@ -6,8 +6,35 @@ import { doc, setDoc } from "firebase/firestore";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import styled from "styled-components";
 import { auth, db } from "../../firebase";
 import { emailMatch, passwordMatch } from "../until";
+
+const Form = styled.form`
+  padding: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  hr {
+    width: 150px;
+  }
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 5px;
+    margin-bottom: 10px;
+  }
+  input {
+    width: 150px;
+    border-radius: 5px;
+  }
+  a {
+    font-size: 14px;
+  }
+`;
 
 export default function SignUp() {
   const [user, setUser] = useState({
@@ -39,35 +66,49 @@ export default function SignUp() {
       })
       .catch((error) => {
         const errorMessage = error.message;
-        console.log(error);
+        if (errorMessage.includes("email-already-in-use")) {
+          window.confirm(
+            "이미 존재하는 이메일입니다. 다른 이메일을 사용해주세요."
+          );
+        }
       });
   };
   return (
-    <form onSubmit={onSubmit}>
-      <label htmlFor="name">name</label>
-      <input
-        type="text"
-        id="name"
-        name="name"
-        onInput={onChange}
-        value={user.name}
-      />
-      <label htmlFor="email">email</label>
-      <input
-        type="text"
-        id="email"
-        name="email"
-        onInput={onChange}
-        value={user.email}
-      />
-      <label htmlFor="password">password</label>
-      <input
-        type="text"
-        id="password"
-        name="password"
-        onInput={onChange}
-        value={user.password}
-      />
+    <Form onSubmit={onSubmit}>
+      <div>
+        <label htmlFor="name">name</label>
+        <input
+          required
+          type="text"
+          id="name"
+          name="name"
+          onInput={onChange}
+          value={user.name}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">email</label>
+        <input
+          required
+          type="text"
+          id="email"
+          name="email"
+          onInput={onChange}
+          value={user.email}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">password</label>
+        <input
+          required
+          type="text"
+          id="password"
+          name="password"
+          onInput={onChange}
+          value={user.password}
+        />
+      </div>
+
       <input
         type="submit"
         value="sign up"
@@ -75,7 +116,8 @@ export default function SignUp() {
           passwordMatch(user.password) && emailMatch(user.email) ? false : true
         }
       />
+      <hr />
       <Link href="/login">Login</Link>
-    </form>
+    </Form>
   );
 }
