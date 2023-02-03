0c1f4bf9980b192bc966344372153079f76ddba1
mamonde456 <mamonde456@gmail.com>
2023-02-03 11:46:29 +0900

    update: 채팅방 스크롤 > 최근 채팅메세지 따라가기

--- renderer/components/chat/Chatting.tsx
+++ renderer/components/chat/Chatting.tsx
@@ -14,7 +14,7 @@ import {
   updateDoc,
 } from "firebase/firestore";
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { auth, db } from "../../../firebase";
 
@@ -29,7 +29,7 @@ const MsgList = styled.ul`
   width: 100%;
   height: 80vh;
   padding: 50px;
-  overflow-y: scroll;
+  overflow-y: auto;
   display: flex;
   flex-direction: column;
 `;
@@ -118,20 +118,28 @@ const DeleteIcon = styled.svg`
   height: 50px;
 `;
 
-export default function Chatting({ roomId, setRoomId, setRooms }) {
+export default function Chatting({ roomId, setRoomId, setIsRemove, setRooms }) {
   const [user, setUser] = useState({
     email: "",
     id: "",
   });
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
+  const scrollRef = useRef<HTMLUListElement>(null);
+  const liRef = useRef<HTMLLIElement>(null);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       setUser({ email: user.email, id: user.uid });
     });
   }, []);
-
+  useEffect(() => {
+    if (!scrollRef && !liRef) return;
+    const ulEl = scrollRef.current;
+    if (ulEl.scrollHeight > 0) {
+      ulEl.scrollTop = ulEl.scrollHeight;
+    }
+  }, [messages]);
   useEffect(() => {
     const newMessageQuery = query(
       collection(db, `room-${roomId}`),
@@ -174,18 +182,20 @@ export default function Chatting({ roomId, setRoomId, setRooms }) {
   };
   const onDelete = async () => {
     try {
-      let chattingMsg = [];
+      let chattingId = [];
       const roomQuery = query(collection(db, `room-${roomId}`));
       onSnapshot(roomQuery, (snapshot) => {
         snapshot.forEach((doc) => {
-          chattingMsg = [...chattingMsg, doc.data().id];
+          console.log(doc.data().id);
+          chattingId = [...chattingId, doc.data().id];
         });
       });
-      chattingMsg.forEach((id) => {
-        deleteDoc(doc(db, "rooms", `${id}`));
+      await chattingId.forEach((id) => {
+        deleteDoc(doc(db, `room-${roomId}`, `${id}`));
       });
       await deleteDoc(doc(db, "rooms", `${roomId}`));
-      setRoomId("");
+      setRoomId(null);
+      setIsRemove((prev) => !prev);
     } catch (error) {
       console.log(error);
     }
@@ -200,9 +210,13 @@ export default function Chatting({ roomId, setRoomId, setRooms }) {
       >
         <path d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L512.9 376.7C552.2 340.2 576 292.3 576 240C576 125.1 461.4 32 320 32c-67.7 0-129.3 21.4-175.1 56.3L38.8 5.1zM64 240c0 45.1 17.7 86.8 47.7 120.9c-1.9 24.5-11.4 46.3-21.4 62.9c-5.5 9.2-11.1 16.6-15.2 21.6c-2.1 2.5-3.7 4.4-4.9 5.7c-.6 .6-1 1.1-1.3 1.4l-.3 .3 0 0 0 0 0 0 0 0c-4.6 4.6-5.9 11.4-3.4 17.4c2.5 6 8.3 9.9 14.8 9.9c28.7 0 57.6-8.9 81.6-19.3c22.9-10 42.4-21.9 54.3-30.6c31.8 11.5 67 17.9 104.1 17.9c37 0 72.3-6.4 104-17.9L82.9 161.3C70.7 185.6 64 212.2 64 240z" />
       </DeleteIcon>
-      <MsgList>
+      <MsgList ref={scrollRef}>
         {messages?.map((el) => (
-          <Li key={el.createdAt} flex={el.owner.name === user.email}>
+          <Li
+            ref={liRef}
+            key={el.createdAt}
+            flex={el.owner.name === user.email}
+          >
             <span className="name">{el.owner.name.split("@")[0]}</span>
             <MessageBox>
               <span
